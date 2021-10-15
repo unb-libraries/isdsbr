7fe8b72bc18fd071299517a1ff741f5931e9acb7
Jacob Sanford <jsanford@unb.ca>
2021-10-15 07:52:31 -0300

    Finalize title transform

--- src/TitleTransformTrait.php
+++ src/TitleTransformTrait.php
@@ -9,17 +9,26 @@ trait TitleTransformTrait {
 
   private function transformTitle() {
     foreach ($this->targetItemElements as $element) {
-      $title_value = '';
-      $this->addLogNotice('Parsing title values');
-      foreach ($element->getElementsByTagName('mods:title') as $spec_element) {
-        $this->addLogNotice('Found Title!');
-        $title_value = $spec_element->textContent;
+      $title_values = [];
+      $subtitle_values = [];
+
+      foreach ($element->childNodes as $spec_element) {
+        if ($spec_element->nodeName == 'mods:title') {
+          $title_values[] = $spec_element->textContent;
+        }
+        if ($spec_element->nodeName == 'mods:subTitle') {
+          $subtitle_values[] = $spec_element->textContent;
+        }
       }
-      foreach ($element->getElementsByTagName('mods:subTitle') as $spec_element) {
-        $this->addLogNotice('Found SubTitle!');
-        $title_value = $title_value . ' | ' . $spec_element->textContent;
+
+      $final_string = implode(" ", $title_values);
+      if (!empty($subtitle_values)) {
+        $subtitle_string = trim(implode(" ", $subtitle_values));
+        if (!empty($subtitle_string)) {
+          $final_string .= ": $subtitle_string";
+        }
       }
-      $this->targetItemValues[] = $title_value;
+      $this->targetItemValues[] = implode(" ", $title_values);
     }
   }
 
