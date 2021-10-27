1ba845099d347fb157d0a3f52d3369961a41aa15
Jacob Sanford <jsanford@unb.ca>
2021-10-27 20:27:36 -0300

    Standardize all time fields based on DSpace format

--- src/DateTransformTrait.php
+++ src/DateTransformTrait.php
@@ -8,14 +8,18 @@ namespace UnbLibraries\IslandoraDspaceBridge;
 trait DateTransformTrait {
 
   private function transform8601DateDefaultNow() {
+    $date_format = 'Y-m-d\TH:i:s\Z';
     foreach ($this->targetItemElements as $element) {
       $mods_value = trim($element->textContent);
+      $tz = date_default_timezone_get();
+      date_default_timezone_set('UTC');
       if (!empty($mods_value)) {
         $timestamp = strtotime($mods_value);
-        $this->targetItemValues[] = date('c', $timestamp);
+        $this->targetItemValues[] = date($date_format, $timestamp);
       } else {
-        $this->targetItemValues[] = date('c', strtotime("now"));
+        $this->targetItemValues[] = date($date_format, strtotime("now"));
       }
+      date_default_timezone_set($tz);
     }
   }
 
